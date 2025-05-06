ebd1a25d42bc0c244c50542176ad77a3eb6253c9
kanannarang <kanannarang22@gmail.com>
2025-05-06 10:35:03 +0530

    Add new file

---
+++ keywordthis.java
@@ -0,0 +1,20 @@
+class Student{
+    int age;
+    Student(int age){
+        this.age = age;
+    }
+    void print(){
+        System.out.println(age);
+    }
+    }
+    class keywordthis{
+public static void main(String[] args) {
+    Student obj = new Student(20);
+    obj.print();
+    
+}
+}
+
+
+
+    
